ac25266af97f27e276377ae0381f6d76595a08fe
Stefan Caras <stefan.caras@gmail.com>
2023-12-22 15:26:53 +0200

    improvements

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-document.querySelector("#stars").addEventListener("animationend", (e) => {
+const $ = (query) => document.querySelector(query);
+
+$("#stars").addEventListener("animationend", (e) => {
   e.target.style.animation = "none";
   e.target.offsetHeight; /* trigger reflow */
   e.target.style = `top: ${Math.floor(Math.random() * 300)}px;
         animation: star ${Math.random() * 2 + 1}s linear 1`;
+});
+
+window.addEventListener("scroll", (e) => {
+  if ($("html").scrollTop > 50) $("#navbar").classList.add("scrolled");
+  else $("#navbar").classList.remove("scrolled");
 });
