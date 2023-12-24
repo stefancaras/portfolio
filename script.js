88a096f0b53b8f09e5ab7ddce05ea0d032dd14f1
Stefan Caras <stefan.caras@gmail.com>
2023-12-24 22:39:31 +0200

    rewrite stars

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const $ = (query) => document.querySelector(query);
 
-$("#stars").addEventListener("animationend", (e) => {
-  e.target.style.animation = "none";
-  e.target.offsetHeight; /* trigger reflow */
-  e.target.style = `top: ${Math.random() * 30}vh;
-        animation: star ${Math.random() + 0.5}s linear 1`;
-});
+// $("#stars").addEventListener("animationend", (e) => {
+//   e.target.style.animation = "none";
+//   e.target.offsetHeight; /* trigger reflow */
+//   e.target.style = `top: ${Math.random() * 30}vh;
+//         right: ${Math.random() * 200 + 8}px;
+//         animation: falling-star ${Math.random() + 0.5}s linear 1`;
+// });
 
 window.addEventListener("scroll", () => {
   if ($("html").scrollTop > 50) $("#navbar").classList.add("scrolled");
